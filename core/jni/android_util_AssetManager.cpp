95326262c5388a449f92e9f785a19c5c6448d6e2
Akhil Narang <akhilnarang.1999@gmail.com>
2016-10-30 23:08:42 +0530

    Revert "Support AOSP service to scan carrier resources"
    This reverts commit 44764ccde343d64233fa8c96881af3f76c595cd7.
    
    We don't need it, also conflicts with OMS, I'm lazy, so just revert :P
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/jni/android_util_AssetManager.cpp
+++ core/jni/android_util_AssetManager.cpp
@@ -28,7 +28,6 @@
 #include <sys/system_properties.h>
 
 #include <private/android_filesystem_config.h> // for AID_SYSTEM
-#include <private/regionalization/Environment.h>
 
 #include "androidfw/Asset.h"
 #include "androidfw/AssetManager.h"
@@ -132,7 +131,7 @@ jint copyValue(JNIEnv* env, jobject outValue, const ResTable* table,
 }
 
 // This is called by zygote (running as user root) as part of preloadResources.
-static void verifySystemIdmaps(const char* overlay_dir)
+static void verifySystemIdmaps()
 {
     pid_t pid;
     char system_id[10];
@@ -187,20 +186,11 @@ static void verifySystemIdmaps(const char* overlay_dir)
                 argv[argc++] = AssetManager::TARGET_APK_PATH;
                 argv[argc++] = AssetManager::IDMAP_DIR;
 
-
-                // Directories to scan for overlays: if OVERLAY_THEME_DIR_PROPERTY is defined,
-                // use OVERLAY_DIR/<value of OVERLAY_THEME_DIR_PROPERTY> in addition to OVERLAY_DIR.
-                char subdir[PROP_VALUE_MAX];
-                int len = __system_property_get(AssetManager::OVERLAY_THEME_DIR_PROPERTY, subdir);
-                if (len > 0) {
-                    String8 overlayPath = String8(AssetManager::OVERLAY_DIR) + "/" + subdir;
-                    if (stat(overlayPath.string(), &st) == 0) {
-                        argv[argc++] = overlayPath.string();
-                    }
-                }
-                if (stat(overlay_dir, &st) == 0) {
-                    argv[argc++] = overlay_dir;
-                }
+                // Directories to scan for overlays
+                // /vendor/overlay
+                if (stat(AssetManager::OVERLAY_DIR, &st) == 0) {
+                    argv[argc++] = AssetManager::OVERLAY_DIR;
+                 }
 
                 // Finally, invoke idmap (if any overlay directory exists)
                 if (argc > 5) {
@@ -2089,20 +2079,7 @@ static jintArray android_content_AssetManager_getStyleAttributes(JNIEnv* env, jo
 static void android_content_AssetManager_init(JNIEnv* env, jobject clazz, jboolean isSystem)
 {
     if (isSystem) {
-        // Load frameworks-res.apk's overlay through regionalization environment
-        if (Environment::isSupported()) {
-            Environment* environment = new Environment();
-            if (environment != NULL) {
-                const char* overlay_dir = environment->getOverlayDir();
-                if (overlay_dir != NULL && strcmp(overlay_dir, "") != 0) {
-                    ALOGD("Regionalization - getOverlayDir:%s", overlay_dir);
-                    verifySystemIdmaps(overlay_dir);
-                }
-                delete environment;
-            }
-        }
-
-        verifySystemIdmaps(AssetManager::OVERLAY_DIR);
+        verifySystemIdmaps();
     }
     AssetManager* am = new AssetManager();
     if (am == NULL) {
